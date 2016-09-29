deebc24c3534a11e5ab834ca9b6b3473b2e8e497
Josh Owens <joshua.owens@gmail.com>
2016-09-29 14:16:39 -0400

    Add the unfollow stuff. Fixes #5

--- imports/api/Methods.js
+++ imports/api/Methods.js
@@ -1,6 +1,13 @@
 Meteor.methods({
   'user/follow': function(followId) {
     check(followId, String);
-    Meteor.users.update(this.userId, {$addToSet: {'profile.followingIds': followId}});
+    if (this.userId)
+      Meteor.users.update(this.userId, {$addToSet: {'profile.followingIds': followId}});
+  },
+  'user/unfollow': function(followId) {
+    check(followId, String);
+    console.log(followId);
+    if (this.userId)
+      Meteor.users.update(this.userId, {$pull: {'profile.followingIds': followId}});
   }
 });
--- imports/ui/components/FollowButton.js
+++ imports/ui/components/FollowButton.js
@@ -5,6 +5,7 @@ class FollowButton extends React.Component {
     super();
     this.following = this.following.bind(this);
     this.followUser = this.followUser.bind(this);
+    this.unfollowUser = this.unfollowUser.bind(this);
   }
 
   following() {
@@ -13,12 +14,17 @@ class FollowButton extends React.Component {
 
   followUser(event) {
     event.preventDefault();
-    Meteor.call('user/follow', this.props.userId)
+    Meteor.call('user/follow', this.props.userId);
+  }
+
+  unfollowUser(event) {
+    event.preventDefault();
+    Meteor.call('user/unfollow', this.props.userId);
   }
 
   render() {
     if (this.following()) {
-      return <Unfollow />
+      return <Unfollow unfollowUser={this.unfollowUser}/>
     } else {
       return <Follow followUser={this.followUser}/>
     }
@@ -29,8 +35,8 @@ const Follow = ({followUser}) => (
   <li className="follow-me"><button onClick={followUser} className="btn btn-primary"><i className="fa fa-plus"></i>&nbsp; Follow</button></li>
 )
 
-const Unfollow = () => (
-  null
+const Unfollow = ({unfollowUser}) => (
+  <li className="follow-me"><button onClick={unfollowUser} className="btn btn-danger"><i className="fa fa-minus"></i>&nbsp; Unfollow</button></li>
 )
 
 export default FollowButton;
